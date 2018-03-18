cbec20a3738654568409b49e9ae9e73331c9afc8
malin4 <2rmalinowski@gmail.com>
2018-03-18 19:21:05 +0100

    tamplates added

--- ui_app/views.py
+++ ui_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Answer
 
-# Create your views here.
+
+def answer_list(request):
+    answers = Answer.published.all()
+    return render(request,
+                  'ui_app/answer.html', {'answers': answers})
+
+
+def answer_detail(request, year, month, day, post):
+    answer = get_object_or_404(Answer, slug=post, status='published', publish__year=year,
+                               publish__month=month, publish__day=day)
+    return render(request,
+                  'ui_app/detail.html', {'answer': answer})
